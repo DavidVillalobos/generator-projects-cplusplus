82464759c04ae4e1e00d05e8af41cf20878d5400
Luis David Villalobos González <villalobosgonzalezluisdavid@gmail.com>
2021-01-09 04:31:00 -0600

    Update init.bat

--- src/init.bat
+++ src/init.bat
@@ -1,22 +1,22 @@
 @ECHO off
 REM File: init.bat
 REM Author: David Villalobos Gonzalez
-REM Date: 01-06-2021
+REM Date: 09-01-2021
 
 REM Size console
 mode con: cols=100 lines=25 
 REM Title console
-title Generate Projects C++ 
+title Generator of Projects C++ 
 
 :Start
 cls
 ECHO.  + - - - - - - - - - - - - - - - +
-ECHO. [93mGenerator of Classes for [96mC++[0m
+ECHO.      Generator of projects [95mC++[0m
 ECHO.                                 
-ECHO.   1. Create Class C++           
-ECHO.   2. Create Makefile            
-ECHO.   3. Exit                       
-ECHO.  + - - - - - - - - - - - - - - - +
+ECHO.          1. [92mCreate Class[0m
+ECHO.          2. [94mCreate Makefile[0m          
+ECHO.          3. [91mExit                      
+ECHO.  [0m+ - - - - - - - - - - - - - - - +
 
 set choice=
 set /p choice= Enter an option :
@@ -34,6 +34,7 @@ python "class_generator.py"
 goto Start
 
 :CreateMakefile
+python "makefile_generator.py"
 goto Start
 
 :Exit
