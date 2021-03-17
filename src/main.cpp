8ba04d35ac69ce110691b08e9e330a69feb7b15c
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-03-17 17:39:07 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*
     File: main.cpp
     Version: 2.0.0
-    Date: 15-03-2021 
+    Date: 17-03-2021 
     Author: David Villalobos
     Description: Main file contains the menu that allows 
     executing the necessary codes to create a project.
@@ -15,7 +15,7 @@ int read_option();
 void generate_main();
 
 int main(){
-    system("mode con: cols=80 lines=25");
+    system("mode con: cols=60 lines=20");
     system("title Generator of Projects C++");
     while(true){
         system("cls");
@@ -25,23 +25,23 @@ int main(){
         std::cout << "      | |  _ / _ \\ '_ \\ / _ \\ '__/ _` | __/ _ \\| '__|\n";
         std::cout << "      | |_| |  __/ | | |  __/ | | (_| | || (_) | |\n";
         std::cout << "       \\____|\\___|_| |_|\\___|_|  \\__,_|\\__\\___/|_|\n";
-        std::cout << "   ____            _           _          [91m____[92m\n";
-        std::cout << "  |  _ \\ _ __ ___ (_) ___  ___| |_ ___   [91m/ ___| _     _[92m\n";
-        std::cout << "  | |_) | '__/ _ \\| |/ _ \\/ __| __/ __| [91m| |   _| |_ _| |_[92m\n";
-        std::cout << "  |  __/| | | (_) | |  __/ (__| |_\\__ \\ [91m| |__|_   _|_   _|[92m\n";
-        std::cout << "  |_|   |_|  \\___// |\\___|\\___|\\__|___/  [91m\\____||_|   |_|[92m\n";
+        std::cout << "   ____            _           _          [95m____[92m\n";
+        std::cout << "  |  _ \\ _ __ ___ (_) ___  ___| |_ ___   [95m/ ___| _     _[92m\n";
+        std::cout << "  | |_) | '__/ _ \\| |/ _ \\/ __| __/ __| [95m| |   _| |_ _| |_[92m\n";
+        std::cout << "  |  __/| | | (_) | |  __/ (__| |_\\__ \\ [95m| |__|_   _|_   _|[92m\n";
+        std::cout << "  |_|   |_|  \\___// |\\___|\\___|\\__|___/  [95m\\____||_|   |_|[92m\n";
         std::cout << "                |__/\n";
-        std::cout << "   Author: https://github.com/DavidVillalobos \n";
-        std::cout << " [0m+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - +\n";
+        std::cout << "   [93mAuthor: https://github.com/DavidVillalobos \n";
+        std::cout << " [95m+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - +\n";
         std::cout << "   1. [94mCreate Class[0m\n";
         std::cout << "   2. [94mCreate Makefile[0m\n";      
         std::cout << "   3. [94mCreate Main[0m\n";
         std::cout << "   4. [91mExit\n"; 
-        std::cout << " [0m+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - +\n";
-        std::cout << "   Enter an option : ";
+        std::cout << " [95m+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - +\n";
+        std::cout << "   [0mEnter an option : ";
         switch(read_option()){
-            case 1: system("genclassc++"); break;
-            case 2: system("genmakec++"); break;
+            case 1: system("cd build & genclassc++"); break;
+            case 2: system("cd build & genmakec++"); break;
             case 3: generate_main(); break;
             case 4: return 0;
             default: std::cout << "Option wrong."; break;
